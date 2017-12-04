c525c981282a35d5175635ccc993bdcc078f9ba8
maaikepull <32471209+maaikepull@users.noreply.github.com>
2017-12-04 10:45:35 -0500

    Update

--- Portfolio website/webpage.js
+++ Portfolio website/webpage.js
@@ -1,13 +1,46 @@
-// Activate Carousel
+
 $("#myCarousel").carousel();
 
     
-// Enable Carousel Indicators
+
 $(".item").click(function(){
     $("#myCarousel").carousel(1);
 });
 
-// Enable Carousel Controls
+
 $(".left").click(function(){
     $("#myCarousel").carousel("prev");
 });
+
+
+$('a.scrollto').on('click', function(){
+  
+
+  var scrollTo = $(this).attr('data-scrollto');
+  
+  
+
+  $( "a.scrollto" ).each(function() {
+    if(scrollTo == $(this).attr('data-scrollto')){
+      $(this).addClass('active');
+    }else{
+      $(this).removeClass('active');
+    }
+  });
+  
+  
+
+  $('body, html').animate({
+    
+
+    "scrolltop": $('#'+scrollTo).offset().top
+  }, 1000 );
+  return false;
+  
+})
+
+$('body').scrollspy({ target: '#navbar-example' })
+
+$('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+})
